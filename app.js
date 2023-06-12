c20b8d293f348ee5c401b5e1a4fcaf69874b7087
parush-uppal <parush1008.be21@chitkara.edu.in>
2023-06-12 19:46:46 +0530

    Email Setup Done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express");
 const morgan = require("morgan");
 require('express-async-errors')
@@ -14,6 +15,7 @@ const { handelNotFound } = require("./utils/helper");
 const app = express();
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'public')))
 app.use(morgan('dev'))
 app.use("/api/user", userRouter);
 app.use("/api/actor", actorRouter);
